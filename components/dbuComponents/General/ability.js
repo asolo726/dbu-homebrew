63954aaea0ab239bb370a6dadda9cfc28b08bebe
asolo726 <demarcoking98@gmail.com>
2025-10-31 19:08:59 -0500

    Slight edits

--- components/dbuComponents/General/ability.js
+++ components/dbuComponents/General/ability.js
@@ -1,11 +1,11 @@
 export default function Ability({ abilityList = [{}] }) {
     return (
-        <div className="mt-4">
+        <div className="mt-2">
             {abilityList.map((item, key) => {
                 if ("condition" in item) {
                     return (
                         <p
-                            className="text-dbu-text text-md md:text-lg text-left my-3"
+                            className="text-dbu-text text-md md:text-lg text-left my-1"
                             key={key}
                         >
                             {"-["}
@@ -22,7 +22,7 @@ export default function Ability({ abilityList = [{}] }) {
                             {item.list.map((listItem, key) => {
                                 return (
                                     <li
-                                        className="my-3 text-dbu-text text-md md:text-lg text-left"
+                                        className="my-2 text-dbu-text text-md md:text-lg text-left"
                                         key={key}
                                     >
                                         {listItem}
--- components/dbuComponents/General/trait.js
+++ components/dbuComponents/General/trait.js
@@ -2,7 +2,7 @@ import Ability from "./ability";
 export default function Trait({traitIntro, abilities}) {
     
     return (
-        <div className="flex-grow-1">
+        <div className="flex-grow-1 mt-2">
             <p className="text-dbu-text text-md md:text-lg text-left">
                 <span className="font-bold text-dbu-header">{traitIntro.title}:</span>{" "}
                 {traitIntro.desc}
