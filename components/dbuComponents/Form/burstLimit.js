2f8f25712c0f810a8c314dccc1bfbec4e2edbd87
asolo726 <dcking4@ilstu.edu>
2025-11-23 16:13:30 -0600

    package and burstlimit style changes

--- components/dbuComponents/Form/burstLimit.js
+++ components/dbuComponents/Form/burstLimit.js
@@ -2,7 +2,7 @@ import Ability from "../General/ability";
 export default function BurstLimit({title = "", desc = "", ability = [{}]}){
     return(
         <div className="mt-10">
-            <p className="text-dbu-header text-center text-md md:text-2xl my-3 font-bold tracking-widest">BURST LIMIT</p>
+            <p className="text-dbu-header text-center text-xl md:text-2xl my-3 font-bold tracking-widest">BURST LIMIT</p>
             <p className="text-dbu-text text-md md:text-lg text-left">
                 <span className="font-bold text-dbu-header">{title}:</span> 
                 {" "}{desc}
