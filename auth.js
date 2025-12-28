b68caaf0d438613bc8e7f08f9e58047eab0c3717
asolo726 <dcking4@ilstu.edu>
2025-12-28 12:18:32 -0600

    Testing if site can deploy without DBAdapter in auth

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import client from "./lib/mongoDBClient"
+// import { MongoDBAdapter } from "@auth/mongodb-adapter"
+// import client from "./lib/mongoDBClient"
 import Okta from "next-auth/providers/okta"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: MongoDBAdapter(client),
+  // adapter: MongoDBAdapter(client),
   providers: [Okta({
     clientId: process.env.OKTA_CLIENT_ID,
     clientSecret: process.env.OKTA_CLIENT_SECRET,
