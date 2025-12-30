9c8859c39e3edb5c7215948ce78ee5e8ea554830
asolo726 <dcking4@ilstu.edu>
2025-12-30 15:39:25 -0600

    testing fix...

--- components/signInorOut.js
+++ components/signInorOut.js
@@ -6,7 +6,6 @@ export default function SignIn({session}) {
     const textStyle = "p-1 mx-4 hover:text-dbu-link hover:text-bold align-center cursor-pointer "
 
     if (session) {
-        console.log(session)
         return (
             <div className="flex flex-col justify-center gap-2">
                 <button 
