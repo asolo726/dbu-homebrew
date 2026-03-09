bb8c8b4640a92c06ad3908b148e183af9c0f3d7c
asolo726 <demarcoking98@gmail.com>
2026-03-09 17:39:24 -0500

    another change

--- components/renderComponents/SinglePageGenerator.js
+++ components/renderComponents/SinglePageGenerator.js
@@ -1,59 +1,6 @@
 import AlternateRenderPage from "./Alternate";
 import AwakeningRenderPage from "./Awakening";
 import LegendaryRenderPage from "./Legendary";
-import searchContent from "../../app/api/searchContent/route";
-
-export async function generateMetadata(resultArr) {
-    // const { slug } = await params;
-    // const resultArr = await searchContent(slug);
-    // console.log(resultArr[0])
-
-    if (resultArr.length < 1) {
-        return {
-            title: "Content Not Found",
-            description:
-                "The content you are looking for does not exist or has not been published yet.",
-        };
-    }
-
-    const result = resultArr;
-
-    const title = result.head.title;
-    const description = result.head.desc;
-    const image = result.head.banner;
-    const url = `https://dbu-homebrew.vercel.app/${result.head.keyName}`;
-
-    return {
-        title,
-        description,
-
-        alternates: {
-            canonical: url,
-        },
-
-        openGraph: {
-            title,
-            description,
-            url,
-            type: "website",
-            images: [
-                {
-                    url: image,
-                    width: 1200,
-                    height: 630,
-                    alt: title,
-                },
-            ],
-        },
-
-        twitter: {
-            card: "summary_large_image",
-            title,
-            description,
-            images: [image],
-        },
-    };
-}
 
 /**
  * Handles generating one page from the [slug] page request. Decides which render method to use depending on the transformationType received.
@@ -61,7 +8,6 @@ export async function generateMetadata(resultArr) {
  */
 
 export default function SinglePageGenerator({ content }) {
-    generateMetadata(content);
     switch (content.head.identity) {
         case "Awakening":
             return (
