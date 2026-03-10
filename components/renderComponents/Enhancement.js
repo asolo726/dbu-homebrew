54b38f1498ede6fdfba9c097d32c3f6c8b89f28b
asolo726 <demarcoking98@gmail.com>
2026-03-09 20:41:16 -0500

    Enhancement and Evolved Stages

---
+++ components/renderComponents/Enhancement.js
@@ -0,0 +1,22 @@
+import Head from "../../components/dbuComponents/Form/head";
+import Trait from "../../components/dbuComponents/General/trait";
+import BurstLimit from "../../components/dbuComponents/Form/burstLimit"
+
+export default function EnhancementRenderPage({ content }) {
+    return (
+        <div className="flex flex-col flex-col-1 max-w-5xl px-10 py-10 md:px-25 sm:m-10 justify-center content-center text-wrap bg-dbu-bg3 sm:rounded-[4em]">
+            <Head Form={content} />
+            {content.traits.map((trait, key) => {
+                return (
+                    <Trait
+                        title={trait.title}
+                        desc={trait.desc}
+                        abilities={trait.abilities}
+                        key={key}
+                    />
+                );
+            })}
+            <BurstLimit burstLimit={content.burstLimit} />
+        </div>
+    );
+}
--- components/renderComponents/SinglePageGenerator.js
+++ components/renderComponents/SinglePageGenerator.js
@@ -1,6 +1,7 @@
 import AlternateRenderPage from "./Alternate";
 import AwakeningRenderPage from "./Awakening";
 import LegendaryRenderPage from "./Legendary";
+import EvolvedStageRenderPage from './EvolvedStage';
 
 /**
  * Handles generating one page from the [slug] page request. Decides which render method to use depending on the transformationType received.
@@ -25,6 +26,18 @@ export default function SinglePageGenerator({ content }) {
             return(
                 <div>
                     <LegendaryRenderPage content={content} />
+                </div>
+            )
+        case "Evolved Stage":
+            return(
+                <div>
+                    <EvolvedStageRenderPage content={content} />
+                </div>
+            )
+        case "Enhancement":
+            return(
+                <div>
+                    <EnhancementRenderPage content={content} />
                 </div>
             )
         case "Temp":
