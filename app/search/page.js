6ea90907216d8a242c906799951c82b7be089949
BlastMan2 <cruzmarc0419@gmail.com>
2026-03-23 12:25:47 -0400

    added metadata for search page

--- app/search/page.js
+++ app/search/page.js
@@ -1,5 +1,23 @@
 import SearchDataServer from "../../components/search/SearchDataServer";
 
+export async function generateMetadata() {
+  return {
+    title: "DBU North Galaxy Homebrew Searcher",
+    description:
+      "Looking for specific homebrew? This page has got you covered. Art by WildBryar.",
+    openGraph: {
+      title: "DBU North Galaxy Homebrew Searcher",
+      description:
+        "Looking for specific homebrew? This page has got you covered. Art by WildBryar.",
+      images:
+        "https://dbu-rpg-northgalaxy.vercel.app/IreallyShouldn'tLetThatSaiyanHitBut_BlastHere_IWantPeopleToKnowThatBryarNamedItThisNotMeIAmNotResponsible.png",
+      type: "website",
+      siteName: "DBU: The Homebrew Galaxy",
+    },
+    authors: [{ name: "Asolo" }, { name: "Blasteroid" }, {name: "WildBryar"}],
+  };
+}
+
 export default function Search() {
     return (
         <div className="self-stretch w-full mr-4">
