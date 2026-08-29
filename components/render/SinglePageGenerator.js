f5fdfc08ff1df2217f6b3d2c32d8514de9756548
BlastMan2 <cruzmarc0419@gmail.com>
2026-08-29 17:40:00 -0400

    Farewell, render files. You served us well

--- components/render/SinglePageGenerator.js
+++ components/render/SinglePageGenerator.js
@@ -1,16 +1,10 @@
-import AlternateRenderPage from "./Alternate";
-import AwakeningRenderPage from "./Awakening";
-import LegendaryRenderPage from "./Legendary";
-import EvolvedStageRenderPage from "./EvolvedStage";
-import EnhancementRenderPage from "./Enhancement";
-import FactorRenderPage from "./Factor";
-import RaceRenderPage from "./Race";
-import OtherRenderPage from "./Other";
 import CommentSection from "../comments/CommentSection";
 import CommunitySettings from "../dbu/General/CommunitySettings";
 import { auth } from "../../auth";
 import { getIsAdmin } from "../../lib/getIsAdmin";
 import { normalizePageContent } from "../../lib/normalizePageContent";
+import Head from "../dbu/General/head";
+import Section from "../dbu/General/Section";
 
 export default async function SinglePageGenerator({ content }) {
 	const session = await auth();
@@ -20,57 +14,37 @@ export default async function SinglePageGenerator({ content }) {
 	const pageRenderStyle =
 		"flex flex-col flex-col-1 w-full max-w-5xl justify-center content-center";
 
-	let pageContent;
-	switch (normalizedContent.data.identity) {
-		case "Awakening":
-			pageContent = <AwakeningRenderPage content={normalizedContent} />;
-			break;
-		case "Alternate":
-			pageContent = <AlternateRenderPage content={normalizedContent} />;
-			break;
-		case "Legendary":
-			pageContent = <LegendaryRenderPage content={normalizedContent} />;
-			break;
-		case "Evolved Stage":
-			pageContent = (
-				<EvolvedStageRenderPage content={normalizedContent} />
-			);
-			break;
-		case "Enhancement":
-			pageContent = <EnhancementRenderPage content={normalizedContent} />;
-			break;
-		case "Factor":
-			pageContent = <FactorRenderPage content={normalizedContent} />;
-			break;
-		case "Race":
-			pageContent = <RaceRenderPage content={normalizedContent} />;
-			break;
-		case "Other":
-			pageContent = <OtherRenderPage content={normalizedContent} />;
-			break;
-		case "Temp":
-			return <></>;
-		default:
-			return <>Something went wrong.</>;
-	}
-
 	return (
 		<div className={pageRenderStyle}>
-			{pageContent}
-			<div className="px-4 pb-4">
-				<CommunitySettings
-					keyName={normalizedContent.data.keyName}
-					isCommunity={normalizedContent.head.isCommunity ?? false}
-				/>
-			</div>
-			<div className="px-4 pb-16">
-				<CommentSection
-					pageKey={normalizedContent.data.keyName}
-					session={session}
-					pageAuthor={normalizedContent.data.author}
-					viewerIsAdmin={isAdmin}
-				/>
-			</div>
+			{normalizedContent ? (
+				<>
+					<div className="px-4 pb-4">
+						<div className="flex flex-col flex-col-1 w-full max-w-5xl mx-auto px-10 py-10 md:px-25 sm:m-10 justify-center content-center text-wrap bg-dbu-bg3 sm:rounded-[4em]">
+							<Head Form={normalizedContent} />
+							<Section
+								body={normalizedContent.body}
+								basePath="body"
+							/>
+						</div>
+						<CommunitySettings
+							keyName={normalizedContent.data.keyName}
+							isCommunity={
+								normalizedContent.head.isCommunity ?? false
+							}
+						/>
+					</div>
+					<div className="px-4 pb-16">
+						<CommentSection
+							pageKey={normalizedContent.data.keyName}
+							session={session}
+							pageAuthor={normalizedContent.data.author}
+							viewerIsAdmin={isAdmin}
+						/>
+					</div>
+				</>
+			) : (
+				<>Something went wrong...</>
+			)}
 		</div>
 	);
 }
